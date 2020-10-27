b5c4d0c636a853652c46cf0159e33d3c44bf3048
Kuang-Yu Jeng <jengku@gmail.com>
2020-10-27 18:14:04 +0800

    add kde model

---
+++ gdn/detector/pointnet2_kde/backbone.py
@@ -0,0 +1,118 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import etw_pytorch_utils as pt_utils
+from collections import namedtuple
+from pointnet2.utils import pointnet2_utils
+from pointnet2.utils.pointnet2_modules import PointnetFPModule
+from pointnet2.utils.pointnet2_modules import PointnetSAModuleMSG
+from ..utils import GatherPoints
+
+
+class Pointnet2MSG(nn.Module):
+    r"""
+        PointNet2 with multi-scale grouping
+        Semantic segmentation network that uses feature propogation layers
+        Parameters
+        ----------
+        num_classes: int
+            Number of semantics classes to predict over -- size of softmax classifier that run for each point
+        input_channels: int = 6
+            Number of input channels in the feature descriptor for each point.  If the point cloud is Nx9, this
+            value should be 6 as in an Nx9 point cloud, 3 of the channels are xyz, and 6 are feature descriptors
+        use_xyz: bool = True
+            Whether or not to use the xyz position of a point as a feature
+    """
+
+    def __init__(self, config, input_channels=0, use_xyz=True, activation_layer=None):
+        super(Pointnet2MSG, self).__init__()
+
+        self.config = config
+        # "subsample_levels": [1024, 256, 64]
+        self.SA_modules = nn.ModuleList()
+        c_in = input_channels
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=config['subsample_levels'][0],
+                radii=[0.01, 0.03],
+                nsamples=[16, 32],
+                mlps=[[c_in, 16, 16, 32], [c_in, 32, 32, 64]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_0 = 32 + 64
+
+        c_in = c_out_0
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=config['subsample_levels'][1],
+                radii=[0.025, 0.05],
+                nsamples=[16, 32],
+                mlps=[[c_in, 64, 64, 128], [c_in, 64, 96, 128]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_1 = 128 + 128
+
+        c_in = c_out_1
+        self.SA_modules.append(
+            PointnetSAModuleMSG(
+                npoint=config['subsample_levels'][2],
+                radii=[0.10, 0.15],
+                nsamples=[16, 32],
+                mlps=[[c_in, 128, 256, 512], [c_in, 128, 256, 512]],
+                use_xyz=use_xyz,
+            )
+        )
+        c_out_2 = 512 + 512
+
+        self.FP_modules = nn.ModuleList()
+        self.FP_modules.append(PointnetFPModule(mlp=[128, 128, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[256+c_out_0, 256, 128]))
+        self.FP_modules.append(PointnetFPModule(mlp=[c_out_2+c_out_1, 256, 256]))
+
+        self.FC_layer = (
+            pt_utils.Seq(128)
+            .conv1d(np.prod(config['output_dim']), activation=None)
+        )
+        self.activation_layer = activation_layer
+
+    def _break_up_pc(self, pc):
+        xyz = pc[..., 0:3].contiguous()
+        features = pc[..., 3:].transpose(1, 2).contiguous() if pc.size(-1) > 3 else None
+
+        return xyz, features
+
+    def forward(self, pointcloud):
+        # type: (Pointnet2MSG, torch.cuda.FloatTensor) -> pt_utils.Seq
+        r"""
+            Forward pass of the network
+            Parameters
+            ----------
+            pointcloud: Variable(torch.cuda.FloatTensor)
+                (B, N, 3 + input_channels) tensor
+                Point cloud to run predicts on
+                Each point in the point-cloud MUST
+                be formated as (x, y, z, features...)
+        """
+        xyz, features = self._break_up_pc(pointcloud)
+
+        l_xyz, l_features = [xyz], [features]
+        for i in range(len(self.SA_modules)):
+            li_xyz, li_features = self.SA_modules[i](l_xyz[i], l_features[i])
+            l_xyz.append(li_xyz)
+            l_features.append(li_features)
+
+        for i in range(-1, -(len(self.FP_modules) + 1), -1):
+            l_features[i - 1] = self.FP_modules[i](
+                l_xyz[i - 1], l_xyz[i], l_features[i - 1], l_features[i]
+            )
+
+        x = self.FC_layer(l_features[0]).transpose(1, 2).contiguous()
+        x = x.view(x.size(0), x.size(1), *self.config['output_dim']) # (B, M, 9)
+        h_feat = x[..., :6] # (B, M, 6)
+        xyz_feat = x[..., 6:9] + xyz # (B, M, 3) -- Learn the residual ...
+        x = torch.cat((h_feat, xyz_feat), 2) # (B, M, 9)
+
+        return self.activation_layer(x)
