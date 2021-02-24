b288ba1cb09efbed261e10cbf1911ce5893dd3f6
Kuang-Yu Jeng <jengku@gmail.com>
2021-02-25 01:46:52 +0800

    Fix None

--- gdn/representation/meta_supervised_baseline/dataset.py
+++ gdn/representation/meta_supervised_baseline/dataset.py
@@ -180,7 +180,8 @@ class collate_fn_setup(object):
                                        config['n_yaw'],
                                        8), dtype=np.float32)
         for b in range(pc_batch.shape[0]):
-            if len(pose_batch[b]) == 0 or len(enclosed_pt_batch[b]) == 0:
+            if pose_batch[b] is None or len(pose_batch[b]) == 0 or \
+               enclosed_pt_batch[b] is None or len(enclosed_pt_batch[b]) == 0:
                 continue
             for n, (pose, ind) in enumerate(zip(pose_batch[b], enclosed_pt_batch[b])):
                 if len(ind)==0:
